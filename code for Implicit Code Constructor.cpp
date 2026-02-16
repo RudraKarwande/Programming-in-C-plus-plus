5c972431db8967be198b26f5a4b0757c834b8c00
Rudra Karwande <232840414+RudraKarwande@users.noreply.github.com>
2026-02-16 21:51:37 +0530

    Create code for Implicit Code Constructor.cpp

---
+++ code for Implicit Code Constructor.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class XYZ {
+    public:
+    string name;
+    XYZ (string n){
+        name =n;
+    }
+};
+int main (){
+    XYZ x1 ("rudra");
+    XYZ x2 =x1;
+     cout << "x1 name: " << x1.name << endl;
+    cout << "x2 name: " << x2.name << endl;
+
+    return 0;
+}
