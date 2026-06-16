9c691d517265e097cc119b4beb7cca37a50129a7
Rudra Karwande <232840414+RudraKarwande@users.noreply.github.com>
2026-06-16 15:38:16 +0530

    check whether no is positive , negative and zero.cpp

---
+++ check whether number is positive ,negative and zero.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int num1 , num2 , num3;
+
+    cout <<"enter three numbers :";
+    cin >> num1 , num2 , num3;
+
+    if (num1 > 0){
+        cout << " num1 is positive:";
+    
+    }
+
+    else if (num1 < 0 < num2){
+     cout <<"num2 is negative:";
+    }
+
+    else {
+        cout << "num3 is zero:";
+    }
+
+    return 0;
+}
